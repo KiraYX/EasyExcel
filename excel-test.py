146b597975c5a4c56bc1321d3039b08d9c48d78e
KiraYX <speedwings@gmail.com>
2020-07-13 22:13:42 +0800

    Read data

--- excel-test.py
+++ excel-test.py
@@ -3,30 +3,25 @@ import xlwt
 from xlutils import copy
 
 workbook = xlrd.open_workbook("阿尔泰.xls")
-names=workbook.sheet_names()
+names = workbook.sheet_names()
 print(names)
 worksheet=workbook.sheet_by_index(0)
 print(worksheet.name)
-
-import win32com.client
-excel = win32com.client.Dispatch('Excel.Application')
-workbook = excel.Workbooks.open(r"C:\Users\think\OneDrive\Kira-Code\EasyExcel\test.xls")
-print(workbook)
-sheet = workbook.WorkSheets('Sheet2')
-print(sheet)
-## get the cell value
-row,col = 1,1
-sheet.Cells(row,col).value
+for r in range(6):
+    for c in range(6):
+        taxpayer = worksheet.cell_value(r, c)
+        print(taxpayer)
+        print(r,c)
 
 
-# excel_path='test.xls'#文件路径
-# #excel_path=unicode('D:\\测试.xls','utf-8')#识别中文路径
-# rbook = xlrd.open_workbook(excel_path,formatting_info=True)#打开文件
-# wbook = copy.copy(rbook)#复制文件并保留格式
-# w_sheet = wbook.get_sheet(1)#索引sheet表
-# row=6
-# col=3
-# value=20180803
+excel_path='财务报表报送与信息采集（适用未执行新金融准则、新收入准则和新租赁准则的一般企业）.xls'#文件路径
+#excel_path=unicode('D:\\测试.xls','utf-8')#识别中文路径
+rbook = xlrd.open_workbook(excel_path,formatting_info=True)#打开文件
+wbook = copy.copy(rbook)#复制文件并保留格式
+w_sheet = wbook.get_sheet(0)#索引sheet表
+row=4
+col=1
+value='G201808033234'
 # w_sheet.write(row,col,value)
 # wbook.save(excel_path)#保存文件
 
