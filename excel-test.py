4dd1e0f328cad24e58aa9eef0058ad83e766019b
KiraYX <speedwings@gmail.com>
2020-07-18 10:21:17 +0800

    weekend

--- excel-test.py
+++ excel-test.py
@@ -5,9 +5,9 @@ import pandas as pd
 import numpy as np
 
 def main():
-    exportFileName = '财务报表报送与信息采集（适用未执行新金融准则、新收入准则和新租赁准则的一般企业）.xls'#文件路径
-    originFileName = '阿尔泰.xls'
-    df = pd.read_excel(originFileName)
+    # destFile = '财务报表报送与信息采集（适用未执行新金融准则、新收入准则和新租赁准则的一般企业）.xls'#文件路径
+    srcFile = '阿尔泰.xls'
+    df = pd.read_excel(srcFile)
     # print(origin)
     cols = list(df.columns)
     print(cols)
