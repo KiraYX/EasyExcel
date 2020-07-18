d3bca5e73f7a9e37b8f9b19e99742a55f5115de3
KiraYX <speedwings@gmail.com>
2020-07-18 22:37:42 +0800

    change to use for loop to solve

--- excel-test.py
+++ excel-test.py
@@ -4,12 +4,98 @@ from xlutils import copy
 import pandas as pd
 import numpy as np
 import os
+from fuzzywuzzy import fuzz as fz
+from fuzzywuzzy import process
 
 def main():
-    cleanData()
+    srcsheet = srcRead()
+    destsheet = destRead()
+    for i in range(1,53):
+        [row,col] = findInTable(srcsheet,str(i))
+        if isValueExist(srcsheet,row,col):
+            # print(srcsheet.cell_value(row,col+1),srcsheet.cell_value(row,col+2))
+            srclabel = getSrcLabel(srcsheet,row,col)
+            print('i='+str(i))
+            print(srclabel)
+            [xpos,ypos] = findLabelInDest(srclabel)
 
+def findLabelInDest(srclabel):
+    pass
+
+def getSrcLabel(sheet,row,col):
+    label = sheet.cell_value(row,col-1)
+    label = label.strip()
+    return label
+
+def isValueExist(sheet,row,col):
+    cell1 = sheet.cell(row,col+1)
+    cell2 = sheet.cell(row,col+2)
+    # value1 = sheet.cell_value(row,col+1)
+    # value2 = sheet.cell_value(row,col+2)
+    # print(cell1,cell2)
+    # print(value1,value2)
+    if cell1.ctype == xlrd.XL_CELL_BLANK and cell2.ctype == xlrd.XL_CELL_BLANK:  
+        return False
+    else:
+        return True
+    
+def findInTable(sheet,key):
+    srcrows = sheet.nrows
+    srccols = sheet.ncols
+    for r in range(0,srcrows):
+        for c in range(0,srccols):
+            if str(sheet.cell_value(r,c)) == key:
+                # print('found index')
+                # print(sheet.cell_value(r,c+1))
+                return [r,c]
+def destRead():
+    destfile = '财务报表报送与信息采集（适用未执行新金融准则、新收入准则和新租赁准则的一般企业）.xls'
+    destbook = xlrd.open_workbook(destfile, formatting_info=True)
+    destsheet = destbook.sheet_by_name("资产负债表")
+    return destsheet
+def srcRead():
+    srcfile = '阿尔泰.xls'
+    srcbook = xlrd.open_workbook(srcfile, formatting_info=True)
+    srcsheet = srcbook.sheet_by_index(0)
+    return srcsheet
+
+def oldmain():
+    pd.set_option('display.max_rows', None)
+    sdf = cleanData()
+    destFile = '财务报表报送与信息采集（适用未执行新金融准则、新收入准则和新租赁准则的一般企业）.xls'
+    rbook = xlrd.open_workbook(destFile, formatting_info=True)
+    names = rbook.sheet_names()
+    print(names)
+    ssheet = rbook.sheet_by_name("资产负债表")
+    nrows = ssheet.nrows
+    ncols = ssheet.ncols
+    print(ssheet.cell_value(5,1))
+    print(nrows,ncols)
+    for i in range(0,nrows):
+        for j in range(0,ncols):
+            if fz.ratio(str(ssheet.cell_value(i,j)),"货币资金") > 99:
+                print(fz.ratio(ssheet.cell_value(i,j),"货币资金"))
+                print("find")
+                print(i,j)
+                r = i
+                c = j
+                print(ssheet.cell_value(i,j))
+    print('out')
+    print(r,c)
+    test = sdf.at['货币资金','periodEnd']
+    print(test)
+    wbook = copy.copy(rbook)
+    wsheet = wbook.get_sheet(1)
+    wsheet.write(r,c+2,test)
+    print(wsheet)
+    wbook.save('output.xls')
+
+    # print(sdf)
+    # test = sdf.iloc[1]
+    # print(test)
+
+    
 def cleanData():
-    # destFile = '财务报表报送与信息采集（适用未执行新金融准则、新收入准则和新租赁准则的一般企业）.xls'#文件路径
     srcFile = '阿尔泰.xls'
     df = pd.read_excel(srcFile)
     findByKey(df, "编制单位")
@@ -26,8 +112,11 @@ def cleanData():
     # print(liabDf)
     dictDF = pd.concat([assetDf,liabDf])
     # print(dictDF)
+    sdf = dictDF
+    sdf['item'] = sdf['item'].str.replace(' ','')
     sdf = dictDF.set_index('item')
-    print(sdf)
+    # print(sdf)
+    return sdf
 
 def findByKey(df, key):
     data = df.values
